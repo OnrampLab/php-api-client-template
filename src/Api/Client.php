a7d1ade73e28271de70fc8b932f4f4a51cd3d752
Eric Chang <ericsonc510@gmail.com>
2023-01-12 18:49:23 +0800

    chore(api): [SampleResource] new

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -3,12 +3,17 @@
 namespace OnrampLab\PhpApiClientTemplate\Api;
 
 use OnrampLab\ApiClient\Api\Client as ApiClient;
+use OnrampLab\PhpApiClientTemplate\Api\Resources\SampleResource;
 
 class Client extends ApiClient
 {
+    public SampleResource $sampleResource;
+
     public function __construct($config)
     {
         $config['baseUrl'] = 'https://www.example.com';
         parent::__construct($config);
+
+        $this->sampleResource = new SampleResource($this);
     }
 }
